3ad92a5d949154226cbdd3f26ec37a586fc65cee
Baran cetin <barcet95@gmail.com>
2018-11-08 00:28:11 -0500

    add plugin description

--- runelite-client/src/main/java/net/runelite/client/plugins/baranplugin/NewPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/baranplugin/NewPlugin.java
@@ -1,4 +1,12 @@
 package net.runelite.client.plugins.baranplugin;
 
+
+import net.runelite.client.plugins.PluginDescriptor;
+
+@PluginDescriptor(
+        name = "Baran Timers",
+        description = "Show baran timers",
+        tags = {"combat", "pve", "overlay", "spawn"}
+)
 public class NewPlugin {
 }
