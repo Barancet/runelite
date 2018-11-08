66eb0fc4e218ca2a63081bab3ecf4e7f05a374b2
Baran cetin <barcet95@gmail.com>
2018-11-08 11:20:26 -0500

    Basic setup of plugin

--- runelite-client/src/main/java/net/runelite/client/plugins/baranplugin/NewPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/baranplugin/NewPlugin.java
@@ -1,6 +1,9 @@
 package net.runelite.client.plugins.baranplugin;
 
 
+import com.google.common.eventbus.Subscribe;
+import net.runelite.api.NPC;
+import net.runelite.api.events.NpcDespawned;
 import net.runelite.client.plugins.PluginDescriptor;
 
 @PluginDescriptor(
@@ -9,4 +12,10 @@ import net.runelite.client.plugins.PluginDescriptor;
         tags = {"combat", "pve", "overlay", "spawn"}
 )
 public class NewPlugin {
-}
+
+          //extend overlay class and add the enum class for monster IDs + names
+    //think of the logic later
+    }
+
+
+
