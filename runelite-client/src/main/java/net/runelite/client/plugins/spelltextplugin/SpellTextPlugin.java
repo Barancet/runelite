2ace320264d15ccb7e96568c7790dcdb05f96480
Baran Cetin <barcet95@gmail.com>
2019-05-11 20:29:31 -0400

    SpellText Plugin

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/spelltextplugin/SpellTextPlugin.java
@@ -0,0 +1,63 @@
+package net.runelite.client.plugins.spelltextplugin;
+
+import net.runelite.api.Client;
+import net.runelite.client.callback.ClientThread;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.overlay.OverlayManager;
+import net.runelite.api.events.MenuOptionClicked;
+
+import javax.inject.Inject;
+
+@PluginDescriptor(
+        name = "Spell Text",
+        description = "Shows what spell you casted in chat box"
+)
+
+public class SpellTextPlugin extends Plugin {
+
+    @Inject
+    private Client client;
+
+    // Make overlay for the SpellTextOverlay Class
+    @Inject
+    private SpellTextOverlay overlay;
+
+    @Inject
+    String test;
+
+
+    //private String menuTarget;
+
+    @Inject
+    private OverlayManager overlayManager;
+
+    @Override
+    protected void startUp() throws Exception
+    {
+        overlayManager.add(overlay);
+    }
+
+    @Override
+    protected void shutDown() throws Exception {
+        super.shutDown();
+        overlayManager.remove(overlay);
+    }
+
+    @Subscribe
+    public void onMenuOptionClicked(MenuOptionClicked event){
+        String mentest = event.getMenuOption();
+        String target = event.getMenuTarget();
+
+        if (mentest.equals("Cast"))
+        {
+            test = event.getMenuTarget();
+            System.out.println(target);
+        }
+
+
+    }
+
+
+}
